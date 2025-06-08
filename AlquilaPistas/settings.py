1102e006056122c6a26b6a53ab751bd39f2231ea
ajimper3009 <ajimper3009@fp.iesromerovargas.com>
2025-06-08 12:27:48 +0200

    Commit para actualizar

--- AlquilaPistas/settings.py
+++ AlquilaPistas/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AlquilaPistas.urls'
@@ -143,6 +144,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'alquila_pistas/static'),  # Aquí van imágenes como logotipos, fondos, etc.
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
